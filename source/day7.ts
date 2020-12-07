c6ac95b9e409087b90e284612d7ac9234d23c45a
Kyle Mayes <kyle@mayeses.com>
2020-12-07 00:43:13 -0500

    Add day 7

---
+++ source/day7.ts
@@ -0,0 +1,66 @@
+import * as _ from "lodash";
+
+import * as input from "./common/input";
+
+const lines = input.lines("day7.txt");
+
+const rules = new Map<string, [number, string][]>();
+
+for (const line of lines) {
+  const [type, requirements] = line.split(" bags contain ");
+  if (requirements !== "no other bags.") {
+    rules.set(
+      type,
+      requirements.split(", ").map((r) => {
+        const space = r.indexOf(" ");
+        const number = parseInt(r.substring(0, space), 10);
+        const type = r.substring(space + 1).replace(/ bag(s)?(\.)?/, "");
+        return [number, type];
+      }),
+    );
+  } else {
+    rules.set(type, []);
+  }
+}
+
+const allowed = new Set<string>();
+
+for (const [type, requirements] of rules.entries()) {
+  if (requirements.find((r) => r[1] === "shiny gold")) {
+    allowed.add(type);
+  }
+}
+
+let added = false;
+while (true) {
+  added = false;
+
+  for (const [type, requirements] of rules.entries()) {
+    if (
+      !allowed.has(type) &&
+      Array.from(allowed).find((a) => requirements.find((r) => r[1] === a))
+    ) {
+      allowed.add(type);
+      added = true;
+    }
+  }
+
+  if (!added) {
+    break;
+  }
+}
+
+console.log(`Part 1: ${allowed.size}`);
+
+let part2 = -1;
+
+let stack: [number, string][] = [[1, "shiny gold"]];
+while (stack.length !== 0) {
+  let [number, type] = stack.pop()!!;
+  part2 += number;
+  for (const requirement of rules.get(type) ?? []) {
+    stack.push([number * requirement[0], requirement[1]]);
+  }
+}
+
+console.log(`Part 2: ${part2}`);
